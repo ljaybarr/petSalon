37f6d93864fa1dba832ff1a4dac71350980ed1d3
ljaybarr <159211043+ljaybarr@users.noreply.github.com>
2024-03-14 17:37:45 -0500

    Add files via upload

---
+++ petSalon/script/display.js
@@ -0,0 +1,112 @@
+let text;
+let list;
+let card;
+let row;
+
+function displayNames(){
+    list = document.getElementById("petList");
+    text="";//clear the var
+    for(let i=0;i<petSalon.pets.length;i++){
+        text+=`<li>${petSalon.pets[i].name}</li>`;
+        console.log("iteration"+ i +" "+ text);
+    }
+    list.innerHTML=text;
+}
+
+function displayCards(){
+    const petsDiv = document.getElementById("pets");
+    card="";
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+        card+=`
+        <div class="petCard ${pet.service.toLowerCase()}">
+            <p>${pet.name}</p>
+            <p>${pet.age}</p>
+            <p>${pet.gender}</p>
+            <p>${pet.breed}</p>
+            <p>${pet.service}</p>
+            <p>${pet.type}</p>
+        </div>
+        `;
+    }
+    petsDiv.innerHTML=card;
+}
+function displayRows(){
+    const TABLE = document.getElementById("petTable");
+    row="";
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+    row+=`
+    <tr id="${i}" class="">
+        <td>${pet.name}</td>
+        <td>${pet.age}</td>
+        <td>${pet.gender}</td>
+        <td>${pet.type}</td>
+        <td>${pet.breed}</td>
+        <td>${pet.service}</td>
+        <td><button class="btn btn-danger" onclick="deletePet(${i});">x</button></td>
+    </tr>
+    `;
+    }
+    TABLE.innerHTML=row;
+}
+function displayTotalAmountPets(){
+    document.getElementById("totalPets").innerHTML=petSalon.pets.length;
+}
+function displayPetType(){
+    //declare variables
+    let dog=0;
+    let cat=0;
+    let bird=0;
+    let hamster=0;
+    let other=0;
+
+    for(let i=0;i<petSalon.pets.length;i++){
+        //travel the array
+        let pet = petSalon.pets[i]; // getting the current pet
+        if(pet.type.toLowerCase() == "dog"){
+            dog++;
+            document.getElementById("totalDog").innerHTML=dog; // display the total grooming on HTML
+        }else if(pet.type.toLowerCase() == "cat"){
+            cat++;
+            document.getElementById("totalCat").innerHTML=cat;
+            //cat
+        }else if(pet.type.toLowerCase() == "bird"){
+            bird++;
+            document.getElementById("totalBird").innerHTML=bird;
+            //bird
+        }else if(pet.type.toLowerCase() == "hamster"){
+            hamster++;
+            document.getElementById("totalHamster").innerHTML=hamster;
+            //hamster
+        }
+        else if(pet.type.toLowerCase() == "other"){
+            other++;
+            document.getElementById("totalOther").innerHTML=other;
+            //other
+        }
+    }
+}
+function displayServiceCount(){
+    //declare variables
+    let grooming=0;
+    let vaccines=0;
+    let hotel=0;
+
+    for(let i=0;i<petSalon.pets.length;i++){
+        //travel the array
+        let pet = petSalon.pets[i]; // getting the current pet
+        if(pet.service.toLowerCase() == "grooming"){
+            grooming++;
+            document.getElementById("totalGrooming").innerHTML=grooming; // display the total grooming on HTML
+        }else if(pet.service.toLowerCase() == "vaccines"){
+            vaccines++;
+            document.getElementById("totalVaccines").innerHTML=vaccines;
+            //vaccines
+        }else{
+            hotel++;
+            document.getElementById("totalHotel").innerHTML=hotel;
+            //hotel
+        }
+    }
+}
--- petSalon/script/register.js
+++ petSalon/script/register.js
@@ -6,45 +6,87 @@ let petSalon = {
         city:"San Diego",
         zip:"12345"
     },
-    pets:[
-        {
-            name:"Scooby",
-            age:80,
-            gender:"Male",
-        },
-        {
-            name:"Scrappy",
-            age:70,
-            gender:"Male",
-        },
-        {
-            name:"Tweety",
-            age:85,
-            gender:"Female",
-        },
-        {
-            name:"Speedy",
-            age:82,
-            gender:"Male",
-        },
-    ]
+    pets:[]
 }
-function displayPetCount(){
+let inputName;
+let inputAge;
+let inputGender;
+let inputType;
+let inputBreed;
+let inputService;
 
+function pet(name, age, gender, type, breed, service){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.type = type;
+    this.breed = breed;
+    this.service = service;
 }
-function displayNames(){
-    
-    for(let i=0;i<petSalon.pets.length;i++){
-        document.getElementById("petList").innerHTML+=`<li>${petSalon.pets[i].name}</li>`;
-    }
-}
-displayNames();
-
 function calculateAverageAge(){
+
     let total=0;
     for(let i=0;i<petSalon.pets.length;i++){
         total = total + petSalon.pets[i].age;
     }
     document.getElementById("petAverageAge").innerHTML+=`<p>${total}</p>`;
 }
-calculateAverageAge();
+function register(){
+    let newPet = new pet(inputName.value,inputAge.value,inputGender.value,inputType.value,inputService.value,inputBreed.value);
+    if(isValid(newPet)){
+    petSalon.pets.push(newPet);
+    //displayCards();
+    displayRows();
+    clearForm();
+    displayTotalAmountPets();
+    displayPetType();
+    displayServiceCount();
+    }
+}
+function isValid(pet){
+    inputName.classList.remove("alert-error");
+    inputService.classList.remove("alert-error");
+    if(!pet.name || !pet.service){
+        if(!pet.name) inputName.classList.add("alert-error");
+        if(!pet.serivce) inputService.classList.add("alert-error");
+        return false;
+    }
+
+    return true;
+}
+function clearForm(){
+    inputName.value=" ";
+    inputAge.value=" ";
+    inputGender.value=" ";
+    inputType.value=" ";
+    inputBreed.value=" ";
+    inputService.value=" ";
+}
+function deletePet(deleteIndex){
+    petSalon.pets.splice(deleteIndex,1);
+    document.getElementById(deleteIndex).remove();
+    displayRows();
+    
+}
+function init(){
+
+    
+    let pet1 = new pet("Scooby", 60, "Male", "Dog", "Great Dane", "Grooming");
+    let pet2 = new pet("Scrappy", 50, "Male", "Dog", "Great Dane", "Vaccines");
+    let pet3 = new pet("Tweety",70,"Female", "Bird", "Yellow Canary", "Hotel");
+    petSalon.pets.push(pet1, pet2, pet3);
+    //displayCards();
+    displayRows();
+    displayTotalAmountPets();
+    displayPetType();
+    displayServiceCount();
+
+    inputName = document.getElementById("txtName");
+    inputAge = document.getElementById("txtAge");
+    inputGender = document.getElementById("txtGender");
+    inputType = document.getElementById("txtType");
+    inputBreed = document.getElementById("txtBreed");
+    inputService = document.getElementById("txtService");
+}
+
+window.onload=init;
